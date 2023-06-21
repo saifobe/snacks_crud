26ea57a2076445b57f8caa58b893c217a9215575
saifobe <obeidatsaif93@gmail.com>
2023-06-21 22:52:44 +0000

    Re-Submit LAB 28

--- snacks/tests.py
+++ snacks/tests.py
@@ -48,3 +48,56 @@ class ThingsTests(TestCase):
         snack = response.context['snack']
         self.assertEqual(snack.name, "rake")
         self.assertEqual(snack.purchaser.username, "tester")
+
+    def test_create_page_status_code(self):
+        url = reverse('snack_create')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_page_status_code(self):
+        url = reverse('snack_delete',args=(1,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_page_status_code(self):
+        url = reverse('snack_update',args=(1,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_page_template(self):
+        url = reverse('snack_update',args=(1,))
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'snack_update.html')
+        self.assertTemplateUsed(response, 'base.html')
+
+    def test_update_page_context(self):
+        url = reverse('snack_update',args=(1,))
+        response = self.client.get(url)
+        snack = response.context['snack']
+        self.assertEqual(snack.name, "rake")
+        self.assertEqual(snack.purchaser.username, "tester")
+
+    def test_create_page_template(self):
+        url = reverse('snack_create')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'snack_create.html')
+        self.assertTemplateUsed(response, 'base.html')
+
+    def test_create_page_context(self):
+        url = reverse('snack_create')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'snack_create.html')
+        self.assertTemplateUsed(response, 'base.html')
+
+    def test_delete_page_template(self):
+        url = reverse('snack_delete',args=(1,))
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'snack_delete.html')
+        self.assertTemplateUsed(response, 'base.html')
+
+    def test_delete_page_context(self):
+        url = reverse('snack_delete',args=(1,))
+        response = self.client.get(url)
+        snack = response.context['snack']
+        self.assertEqual(snack.name, "rake")
+        self.assertEqual(snack.purchaser.username, "tester")
